9f2de952fd2f8271e6f8667828f16bbd25d57cb3
huanghoufeng <hofe.cn@gmail.com>
2024-12-19 15:18:25 +0800

    扩展electron支持

---
+++ main.js
@@ -0,0 +1,34 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1200,
+    height: 1000,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js'),
+      contextIsolation: true,
+      enableRemoteModule: false,
+    },
+  });
+
+  win.loadURL(
+    // process.env.ELECTRON_START_URL || `file://${path.join(__dirname, '/build/index.html')}`
+    // `file://${path.join(__dirname, '/build/index.html')}`
+    'http://localhost:3000'
+  );
+}
+
+app.on('ready', createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+}); 
